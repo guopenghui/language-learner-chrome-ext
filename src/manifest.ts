93f5e6d17540c1c85d05398ce8f7359a3c671f85
guopenghui <guopeng_hui@126.com>
2022-11-15 00:31:28 +0800

    调整缩进

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,39 +1,39 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
 export default defineManifest({
-  name: 'langr',
-  description: '',
-  version: '0.0.0',
-  manifest_version: 3,
-  icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
-  },
-  action: {
-    default_popup: 'popup.html',
-    default_title: 'Click Me',
-    default_icon: 'img/logo-48.png',
-  },
-  options_page: 'options.html',
-  background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module',
-  },
-  content_scripts: [
-    {
-      // matches: ['http://*/*', 'https://*/*'],
-      matches: ['<all_urls>'],
-      js: ['src/content/index.ts'],
+    name: 'langr',
+    description: '',
+    version: '0.0.0',
+    manifest_version: 3,
+    icons: {
+        16: 'img/logo-16.png',
+        32: 'img/logo-34.png',
+        48: 'img/logo-48.png',
+        128: 'img/logo-128.png',
     },
-  ],
-  web_accessible_resources: [
-    {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
-      matches: [],
+    action: {
+        default_popup: 'popup.html',
+        default_title: 'Click Me',
+        default_icon: 'img/logo-48.png',
     },
-  ],
-  permissions: ["storage", "contextMenus", "tabs"],
-  host_permissions: ["http://localhost:*/*"],
+    options_page: 'options.html',
+    background: {
+        service_worker: 'src/background/index.ts',
+        type: 'module',
+    },
+    content_scripts: [
+        {
+            // matches: ['http://*/*', 'https://*/*'],
+            matches: ['<all_urls>'],
+            js: ['src/content/index.ts'],
+        },
+    ],
+    web_accessible_resources: [
+        {
+            resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+            matches: [],
+        },
+    ],
+    permissions: ["storage", "contextMenus", "tabs"],
+    host_permissions: ["http://localhost:*/*"],
 })
--- src/options/Options.vue
+++ src/options/Options.vue
@@ -5,19 +5,19 @@ import { t } from "../lang/helper"
 
 let port = ref(3002)
 chrome.storage.sync.get(["port"], (data) => {
-  port.value = data.port
+    port.value = data.port
 })
 
 let message = useMessage()
 
 
 function saveSettings() {
-  chrome.storage.sync.set({
-    port: port.value,
-  }, () => {
-    chrome.runtime.sendMessage({type: "CHANGE_PORT", port: port.value})
-    message.success(t("Save Success"))
-  })
+    chrome.storage.sync.set({
+        port: port.value,
+    }, () => {
+        chrome.runtime.sendMessage({type: "CHANGE_PORT", port: port.value})
+        message.success(t("Save Success"))
+    })
 }
 
 // function MC(props:any, { slots, emit, attrs}: any) {
@@ -27,58 +27,58 @@ function saveSettings() {
 </script>
 
 <template>
-  <main>
-    <h3>Options Page!</h3>
-    <div class="option-container">
-      <span>{{t("Port")}}: </span>
-      <input type="number" v-model="port">
-      <button @click="saveSettings">Save</button>
-    </div>
-  </main>
+    <main>
+        <h3>Options Page!</h3>
+        <div class="option-container">
+            <span>{{t("Port")}}: </span>
+            <input type="number" v-model="port">
+            <button @click="saveSettings">Save</button>
+        </div>
+    </main>
 </template>
 
 <style>
 main {
-  text-align: center;
-  padding: 1em;
-  margin: 0 auto;
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
 }
 
 h3 {
-  color: #42b983;
-  text-transform: uppercase;
-  font-size: 1.5rem;
-  font-weight: 200;
-  line-height: 1.2rem;
-  margin: 2rem auto;
+    color: #42b983;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    font-weight: 200;
+    line-height: 1.2rem;
+    margin: 2rem auto;
 }
 
 h6 {
-  font-size: 0.5rem;
-  color: #999999;
-  margin: 0.5rem;
+    font-size: 0.5rem;
+    color: #999999;
+    margin: 0.5rem;
 }
 
 a {
-  font-size: 0.5rem;
-  margin: 0.5rem;
-  color: #cccccc;
-  text-decoration: none;
+    font-size: 0.5rem;
+    margin: 0.5rem;
+    color: #cccccc;
+    text-decoration: none;
 }
 
 @media (min-width: 480px) {
-  h3 {
-    max-width: none;
-  }
+    h3 {
+        max-width: none;
+    }
 }
 
 @media (prefers-color-scheme: light) {
-  a:hover {
-    color: #747bff;
-  }
+    a:hover {
+        color: #747bff;
+    }
 
-  h6 {
-    color: #333333;
-  }
+    h6 {
+        color: #333333;
+    }
 }
 </style>
